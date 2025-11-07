5046fd953ef5db04d58594f9d7cf387e497e10cd
steschu77 <steschu77@gmail.com>
2025-11-07 15:54:48 +0800

    Remove arithmetic FastDecoder

--- src/vp8_arithmetic_decoder.rs
+++ src/vp8_arithmetic_decoder.rs
@@ -52,13 +52,6 @@ struct State {
     bit_count: i32,
 }
 
-#[cfg_attr(test, derive(Debug))]
-struct FastDecoder<'a> {
-    chunks: &'a [[u8; 4]],
-    uncommitted_state: State,
-    save_state: &'a mut State,
-}
-
 impl ArithmeticDecoder {
     pub(crate) fn new() -> ArithmeticDecoder {
         let state = State {
@@ -155,85 +148,33 @@ impl ArithmeticDecoder {
         BitResult::ok(value_if_not_past_eof)
     }
 
-    // Do not inline this because inlining seems to worsen performance.
-    #[inline(never)]
     pub(crate) fn read_bool(&mut self, probability: u8) -> BitResult<bool> {
-        if let Some(b) = self.fast().read_bool(probability) {
-            return BitResult::ok(b);
-        }
-
         self.cold_read_bool(probability)
     }
 
-    // Do not inline this because inlining seems to worsen performance.
-    #[inline(never)]
     pub(crate) fn read_flag(&mut self) -> BitResult<bool> {
-        if let Some(b) = self.fast().read_flag() {
-            return BitResult::ok(b);
-        }
-
         self.cold_read_flag()
     }
 
-    // Do not inline this because inlining seems to worsen performance.
-    #[inline(never)]
     pub(crate) fn read_literal(&mut self, n: u8) -> BitResult<u8> {
-        if let Some(v) = self.fast().read_literal(n) {
-            return BitResult::ok(v);
-        }
-
         self.cold_read_literal(n)
     }
 
-    // Do not inline this because inlining seems to worsen performance.
-    #[inline(never)]
     pub(crate) fn read_optional_signed_value(&mut self, n: u8) -> BitResult<i32> {
-        if let Some(v) = self.fast().read_optional_signed_value(n) {
-            return BitResult::ok(v);
-        }
-
         self.cold_read_optional_signed_value(n)
     }
 
-    // This is generic and inlined just to skip the first bounds check.
-    #[inline]
     pub(crate) fn read_with_tree<const N: usize>(&mut self, tree: &[TreeNode; N]) -> BitResult<i8> {
         let first_node = tree[0];
         self.read_with_tree_with_first_node(tree, first_node)
     }
 
-    // Do not inline this because inlining significantly worsens performance.
-    #[inline(never)]
     pub(crate) fn read_with_tree_with_first_node(
         &mut self,
         tree: &[TreeNode],
         first_node: TreeNode,
     ) -> BitResult<i8> {
-        if let Some(v) = self.fast().read_with_tree(tree, first_node) {
-            return BitResult::ok(v);
-        }
-
         self.cold_read_with_tree(tree, usize::from(first_node.index))
-    }
-
-    // As a similar (but different) speedup to BitResult, the FastDecoder reads
-    // bits under an assumption and validates it at the end.
-    //
-    // The idea here is that for normal-sized webp images, the vast majority
-    // of bits are somewhere other than in the last four bytes. Therefore we
-    // can pretend the buffer has infinite size. After we are done reading,
-    // we check if we actually read past the end of `self.chunks`.
-    // If so, we backtrack (or rather we discard `uncommitted_state`)
-    // and try again with the slow approach. This might result in doing double
-    // work for those last few bytes -- in fact we even keep retrying the fast
-    // method to save an if-statement --, but more than make up for that by
-    // speeding up reading from the other thousands or millions of bytes.
-    fn fast(&mut self) -> FastDecoder<'_> {
-        FastDecoder {
-            chunks: &self.chunks,
-            uncommitted_state: self.state,
-            save_state: &mut self.state,
-        }
     }
 
     const FINAL_BYTES_REMAINING_EOF: i8 = -0xE;
@@ -309,20 +250,14 @@ impl ArithmeticDecoder {
         BitResult::ok(retval)
     }
 
-    #[cold]
-    #[inline(never)]
     fn cold_read_bool(&mut self, probability: u8) -> BitResult<bool> {
         self.cold_read_bit(probability)
     }
 
-    #[cold]
-    #[inline(never)]
     fn cold_read_flag(&mut self) -> BitResult<bool> {
         self.cold_read_bit(128)
     }
 
-    #[cold]
-    #[inline(never)]
     fn cold_read_literal(&mut self, n: u8) -> BitResult<u8> {
         let mut v = 0u8;
         let mut res = self.start_accumulated_result();
@@ -335,8 +270,6 @@ impl ArithmeticDecoder {
         self.keep_accumulating(res, v)
     }
 
-    #[cold]
-    #[inline(never)]
     fn cold_read_optional_signed_value(&mut self, n: u8) -> BitResult<i32> {
         let mut res = self.start_accumulated_result();
         let flag = self.cold_read_flag().or_accumulate(&mut res);
@@ -355,8 +288,6 @@ impl ArithmeticDecoder {
         self.keep_accumulating(res, value)
     }
 
-    #[cold]
-    #[inline(never)]
     fn cold_read_with_tree(&mut self, tree: &[TreeNode], start: usize) -> BitResult<i8> {
         let mut index = start;
         let mut res = self.start_accumulated_result();
@@ -373,188 +304,6 @@ impl ArithmeticDecoder {
                 let value = TreeNode::value_from_branch(t);
                 return self.keep_accumulating(res, value);
             }
-        }
-    }
-}
-
-impl FastDecoder<'_> {
-    fn commit_if_valid<T>(self, value_if_not_past_eof: T) -> Option<T> {
-        // If `chunk_index > self.chunks.len()`, it means we used zeroes
-        // instead of an actual chunk and `value_if_not_past_eof` is nonsense.
-        if self.uncommitted_state.chunk_index <= self.chunks.len() {
-            *self.save_state = self.uncommitted_state;
-            Some(value_if_not_past_eof)
-        } else {
-            None
-        }
-    }
-
-    fn read_bool(mut self, probability: u8) -> Option<bool> {
-        let bit = self.fast_read_bit(probability);
-        self.commit_if_valid(bit)
-    }
-
-    fn read_flag(mut self) -> Option<bool> {
-        let value = self.fast_read_flag();
-        self.commit_if_valid(value)
-    }
-
-    fn read_literal(mut self, n: u8) -> Option<u8> {
-        let value = self.fast_read_literal(n);
-        self.commit_if_valid(value)
-    }
-
-    fn read_optional_signed_value(mut self, n: u8) -> Option<i32> {
-        let flag = self.fast_read_flag();
-        if !flag {
-            // We should not read further bits if the flag is not set.
-            return self.commit_if_valid(0);
-        }
-        let magnitude = self.fast_read_literal(n);
-        let sign = self.fast_read_flag();
-        let value = if sign {
-            -i32::from(magnitude)
-        } else {
-            i32::from(magnitude)
-        };
-        self.commit_if_valid(value)
-    }
-
-    fn read_with_tree(mut self, tree: &[TreeNode], first_node: TreeNode) -> Option<i8> {
-        let value = self.fast_read_with_tree(tree, first_node);
-        self.commit_if_valid(value)
-    }
-
-    fn fast_read_bit(&mut self, probability: u8) -> bool {
-        let State {
-            mut chunk_index,
-            mut value,
-            mut range,
-            mut bit_count,
-        } = self.uncommitted_state;
-
-        if bit_count < 0 {
-            let chunk = self.chunks.get(chunk_index).copied();
-            // We ignore invalid data inside the `fast_` functions,
-            // but we increase `chunk_index` below, so we can check
-            // whether we read invalid data in `commit_if_valid`.
-            let chunk = chunk.unwrap_or_default();
-
-            let v = u32::from_be_bytes(chunk);
-            chunk_index += 1;
-            value <<= 32;
-            value |= u64::from(v);
-            bit_count += 32;
-        }
-        debug_assert!(bit_count >= 0);
-
-        let probability = u32::from(probability);
-        let split = 1 + (((range - 1) * probability) >> 8);
-        let bigsplit = u64::from(split) << bit_count;
-
-        let retval = if let Some(new_value) = value.checked_sub(bigsplit) {
-            range -= split;
-            value = new_value;
-            true
-        } else {
-            range = split;
-            false
-        };
-        debug_assert!(range > 0);
-
-        // Compute shift required to satisfy `range >= 128`.
-        // Apply that shift to `range` and `self.bitcount`.
-        //
-        // Subtract 24 because we only care about leading zeros in the
-        // lowest byte of `range` which is a `u32`.
-        let shift = range.leading_zeros().saturating_sub(24);
-        range <<= shift;
-        bit_count -= shift as i32;
-        debug_assert!(range >= 128);
-
-        self.uncommitted_state = State {
-            chunk_index,
-            value,
-            range,
-            bit_count,
-        };
-        retval
-    }
-
-    fn fast_read_flag(&mut self) -> bool {
-        let State {
-            mut chunk_index,
-            mut value,
-            mut range,
-            mut bit_count,
-        } = self.uncommitted_state;
-
-        if bit_count < 0 {
-            let chunk = self.chunks.get(chunk_index).copied();
-            // We ignore invalid data inside the `fast_` functions,
-            // but we increase `chunk_index` below, so we can check
-            // whether we read invalid data in `commit_if_valid`.
-            let chunk = chunk.unwrap_or_default();
-
-            let v = u32::from_be_bytes(chunk);
-            chunk_index += 1;
-            value <<= 32;
-            value |= u64::from(v);
-            bit_count += 32;
-        }
-        debug_assert!(bit_count >= 0);
-
-        let half_range = range / 2;
-        let split = range - half_range;
-        let bigsplit = u64::from(split) << bit_count;
-
-        let retval = if let Some(new_value) = value.checked_sub(bigsplit) {
-            range = half_range;
-            value = new_value;
-            true
-        } else {
-            range = split;
-            false
-        };
-        debug_assert!(range > 0);
-
-        // Compute shift required to satisfy `range >= 128`.
-        // Apply that shift to `range` and `self.bitcount`.
-        //
-        // Subtract 24 because we only care about leading zeros in the
-        // lowest byte of `range` which is a `u32`.
-        let shift = range.leading_zeros().saturating_sub(24);
-        range <<= shift;
-        bit_count -= shift as i32;
-        debug_assert!(range >= 128);
-
-        self.uncommitted_state = State {
-            chunk_index,
-            value,
-            range,
-            bit_count,
-        };
-        retval
-    }
-
-    fn fast_read_literal(&mut self, n: u8) -> u8 {
-        let mut v = 0u8;
-        for _ in 0..n {
-            let b = self.fast_read_flag();
-            v = (v << 1) + u8::from(b);
-        }
-        v
-    }
-
-    fn fast_read_with_tree(&mut self, tree: &[TreeNode], mut node: TreeNode) -> i8 {
-        loop {
-            let prob = node.prob;
-            let b = self.fast_read_bit(prob);
-            let i = if b { node.right } else { node.left };
-            let Some(next_node) = tree.get(usize::from(i)) else {
-                return TreeNode::value_from_branch(i);
-            };
-            node = *next_node;
         }
     }
 }
